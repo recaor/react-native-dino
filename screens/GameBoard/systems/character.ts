c707f32103000cf82ff71b40a0b830cfe9db47fe
recaor <redcarrot0803@gmail.com>
2022-08-27 06:29:59 -0230

    feat: Add score calculation

--- screens/GameBoard/systems/character.ts
+++ screens/GameBoard/systems/character.ts
@@ -1,11 +1,15 @@
 import Matter from "matter-js";
 import { Dimensions } from "react-native";
+import { FLOOR_Y } from "../utils/constants";
 import { getRandom } from "../utils/random";
 
 const { width } = Dimensions.get("screen");
 let isFlying = false;
+let collisionNo = 0;
 
 const UpdateCharacter = (entities, { dispatch, touches, time }) => {
+  if (entities["Fruit"].body.position.x > width) collisionNo = 0;
+
   const engine = entities.physics.engine;
   touches
     .filter((t) => t.type === "press")
@@ -34,12 +38,18 @@ const UpdateCharacter = (entities, { dispatch, touches, time }) => {
       (collisionA === "character" && collisionB === "obstacle")
     ) {
       dispatch({ type: "game_over" });
-    } else if (
-      (collisionB === "character" && collisionA === "fruit") ||
-      (collisionA === "character" && collisionB === "fruit")
+    }
+    if (
+      (collisionB === "character" &&
+        collisionA === "fruit" &&
+        collisionNo === 0) ||
+      (collisionA === "character" &&
+        collisionB === "fruit" &&
+        collisionNo === 0)
     ) {
       const newPosition = getRandom(width + 200, width + 1500);
       Matter.Body.translate(entities["Fruit"].body, { x: newPosition, y: 0 });
+      collisionNo++;
       dispatch({ type: "score" });
     }
   });
